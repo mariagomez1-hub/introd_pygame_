72959071d842f2b952feb8ca58eb5e1f90da9851
mariagomez1 <mariagomez1@colegioguanenta.edu.co>
2026-05-06 07:39:31 -0500

    A

--- estructura_basica.py
+++ estructura_basica.py
@@ -12,15 +12,31 @@ pygame.display.set_caption("Pygame Guanenta")
 
 # definir colores
 azul = (0,0,255)
+rojo = (255, 0, 0)
+verde = (0,255,0)
+amarillo = (255,255,0)
+blanco = (255,255,255)
 
 # creamos una superficie
-superficie_1 = pygame.surface((400,400))
+superficie_1 = pygame.surface((200,200))
+superficie_2 = pygame.surface((200,200))
+superficie_3 = pygame.surface((200,200))
+superficie_4 = pygame.surface((200,200))
+superficie_5 = pygame.surface((200,200))
 
 # rellenamos la superficie de color
 superficie_1.fill(azul)
+superficie_2.fill(rojo)
+superficie_3.fill(verde)
+superficie_4.fill(amarillo)
+superficie_5.fill(blanco)
 
 # agregar o mover la superficie de la ventana
 ventana.blit(Superficie_1, (0,0))
+ventana.blit(Superficie_2, (200,0))
+ventana.blit(Superficie_3, (0,200))
+ventana.blit(Superficie_4, (200,200))
+ventana.blit(Superficie_5, (100,100))
 # actualizar visualizacion de la ventana
 pygame.display.flip()
 
